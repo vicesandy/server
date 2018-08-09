b10e006ee328067b950ccc8d7a860b1e3382455e
sam <sam@wlcl254118.wlcl.siu.edu>
2018-08-09 15:59:11 -0500

    package.json

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,18 +1,21 @@
-import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+import {Entity, PrimaryColumn, Column, BeforeInsert} from "typeorm";
+import * as uuidv4 from "uuid/v4";
 
 @Entity()
 export class User {
 
-    @PrimaryGeneratedColumn()
-    id: number;
+    @PrimaryColumn("uuid")
+    id: string;
 
     @Column()
-    firstName: string;
+    email: string;
 
     @Column()
-    lastName: string;
+    password: string;
 
-    @Column()
-    age: number;
+    @BeforeInsert()
+    addId() {
+    	this.id = uuidv4();
+    }
 
 }
